957df864caba754525b9355305c970978ee3f106
sethvincent <sethvincent@gmail.com>
2016-09-08 11:10:50 -0700

    fix example

--- examples/http.js
+++ examples/http.js
@@ -4,7 +4,7 @@ var local = require('wzrd-bundler/local-module')
 var code = `var html = require('yo-yo')
 var request = require('xhr')
 
-request('http://boundaries.seattle.io/boundaries', function (err, res, body) {
+request('http://boundaries-api.seattle.io/boundaries', function (err, res, body) {
   console.log(body)
   var content = html\`<p>\${body}</p>\`
   document.body.appendChild(content)
